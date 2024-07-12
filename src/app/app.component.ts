15aa0e4ac83f185e19158f86a7a2df99f79f6738
Rahhhh <meow@nermalcat69.dev>
2024-07-12 17:23:33 +0530

    chore: remove unnecessary import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TextsComponent } from './texts/texts.component';
 
 @Component({
   selector: 'app-root',
